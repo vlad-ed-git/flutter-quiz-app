8e05c83ad6bb9531a08bb1291ca1847a4c5af188
Vladimir Peter <vlad.edna@gmail.com>
2022-01-27 22:03:13 +0800

    setup firebase with flutterfire

---
+++ android/app/src/main/kotlin/com/dev_vlad/quizme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dev_vlad.quizme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Firebase init error');
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const Text('Firebase initialized');
+        }
+
+        return const Text('Loading Firebase Init');
+      },
+    );
+  }
+}
